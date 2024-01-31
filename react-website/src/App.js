38e0a3c4e07f9759fcbf6322623a58c4e712c31f
morganbroome <guymorgan100@gmail.com>
2024-01-30 20:18:36 -0500

    Abstracted pages into page framework

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,28 +1,36 @@
 import React from "react";
+
+// Components
 import Navbar from "./components/Navbar";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+// Pages
 import Home from "./pages";
 import About from "./pages/about";
 import Blogs from "./pages/blogs";
 import SignUp from "./pages/signup";
 import Contact from "./pages/contact";
+import PageFramework from "./pages/framework";
  
 function App() {
     return (
         <Router>
-            <Navbar />
             <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
+                <Route 
+                    exact path="/" 
+                    element={<PageFramework component={<Home/>} />} />
+                <Route 
+                    path="/about" 
+                    element={<PageFramework component={<About/>} />} />
                 <Route
                     path="/contact"
-                    element={<Contact />}
-                />
-                <Route path="/blogs" element={<Blogs />} />
+                    element={<PageFramework component={<Contact/>} />} />
+                <Route 
+                    path="/blogs" 
+                    element={<PageFramework component={<Blogs/>} />} />
                 <Route
                     path="/sign-up"
-                    element={<SignUp />}
-                />
+                    element={<PageFramework component={<SignUp/>} />} />
             </Routes>
         </Router>
     );
